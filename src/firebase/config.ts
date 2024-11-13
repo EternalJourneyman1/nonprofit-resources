c546e52df0828b0a0b61f51c984c9a401ae844b4
Demetrious Robinson <demetrious@innercode.org>
2024-11-12 23:43:36 -0800

    Add firbaseConfig directly

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAnalytics, logEvent, type Analytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyDyIXc44BB1fSu5gQkCsKP_sfqkd7H3U0Q",
+  authDomain: "nonprofit-resources.firebaseapp.com",
+  projectId: "nonprofit-resources",
+  storageBucket: "nonprofit-resources.firebasestorage.app",
+  messagingSenderId: "1034770576350",
+  appId: "1:1034770576350:web:c3265aee972712c015e34f",
+  measurementId: "G-JMPTX7QCK6"
 };
 
 const app = initializeApp(firebaseConfig);
